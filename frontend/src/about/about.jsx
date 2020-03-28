4077217c603d843a959aa657fdd5f5c0f564a027
Breno Lemos <brenomiros@gmail.com>
2020-03-28 15:36:08 -0300

    Componente PageHeader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import PageHeader from "../template/pageHeader";
 
 export default props => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name="Sobre" small="Nós"></PageHeader>
+    <h3>Nossa História</h3>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora culpa
+      pariatur natus distinctio, atque, dignissimos officia, ipsam accusantium
+      totam maiores fugit eos praesentium dolores excepturi?
+    </p>
+
+    <h3>Missão e Visão</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, illo?
+    </p>
+
+    <h3>Imprensa</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, illo?
+    </p>
   </div>
 );
